1cf024aceec149cd11edb212187fee9996323e2a
msabbar02 <149406843+msabbar02@users.noreply.github.com>
2024-04-29 17:34:27 +0200

    Add files via upload

--- trabajadores/demo/src/main/java/org/example/demo/HelloController.java
+++ trabajadores/demo/src/main/java/org/example/demo/HelloController.java
@@ -3,9 +3,48 @@ package org.example.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloController {
+    @FXML
+    PasswordField loginContraseña;
+    @FXML
+    TextField loginNombre;
+    @FXML
+    Button botonLogin;
+    @FXML
+    CheckBox checkBoxUsuario;
+
+    // Simulación de una base de datos
+
+    public HelloController() {
+
+    }
+
+    @FXML
+    public void initialize() {
+        // Aquí puedes realizar inicializaciones adicionales
+        // Por ejemplo, podrías deseleccionar el CheckBox
+        checkBoxUsuario.setSelected(false);
+
+        // También podrías establecer un evento de acción para el botón de inicio de sesión
+        botonLogin.setOnAction(event -> login());
+
+        // Si deseas cargar datos adicionales o realizar otras tareas de inicialización,
+        // este es el lugar adecuado para hacerlo
+    }
+
+    @FXML
+    public void login() {
+        ArrayList<Usuario> usuarios= MYSQL.crearUsuarios();
+        if (MYSQL.checkusuario(usuarios,loginNombre.toString(),loginContraseña.toString())){
+
+
+        }else{
+
+        }
+
+    }
 }
