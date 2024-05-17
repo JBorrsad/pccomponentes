d52650107f6eba49ee0e95a5f6af3ba1b97d1fc3
msabbar02 <149406843+msabbar02@users.noreply.github.com>
2024-05-17 15:32:35 +0200

    Add files via upload
    LAST VERSION

---
+++ Proyectoak/PCcomponentes/src/main/java/PCcomponentes/Productos/TestController.java
@@ -0,0 +1,17 @@
+package PCcomponentes.Productos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+public class TestController {
+
+    @FXML
+    TextField test;
+
+
+    public TestController(){
+        test.setText("TEST");
+    }
+
+}
